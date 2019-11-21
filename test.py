0e1eef4960c6d6ec50e128a3381e74221eb14f27
T.G.A van Hasselt <tgavanhasselt@MBP-van-TGA.fios-router.home>
2019-11-21 10:30:03 -0500

    Simple test python file

---
+++ test.py
@@ -0,0 +1,8 @@
+# This is a python file which will mainly be used for the Coursera Capstone course
+# My name is Tim van Hasselt
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
